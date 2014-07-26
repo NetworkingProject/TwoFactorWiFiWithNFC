7157bfde717c3dadbd1b2404e63fa6df78d123e4
Patrick McGee <pmcgee@twitter.com>
2014-07-26 11:49:12 -0400

    more serilization changes

--- src/com/example/nfcproject/ConfigSerialization.java
+++ src/com/example/nfcproject/ConfigSerialization.java
@@ -1,5 +1,8 @@
 package com.example.nfcproject;
 
+import android.net.wifi.WifiConfiguration;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -12,13 +15,27 @@ public class ConfigSerialization implements Parcelable {
     public int keyManagement; // This is 0-3
 
     // With Secondary Password
-    public ConfigSerialization(String sSID, String password, String secondaryPassword, boolean isTwoFactor, boolean isHidden, int keyMangement) {
+    public ConfigSerialization(String sSID, String password,
+            String secondaryPassword, boolean isTwoFactor, boolean isHidden,
+            int keyMangement) {
         this.SSID = sSID;
         this.password = password;
         this.secondaryPassword = secondaryPassword;
         this.isTwoFactor = isTwoFactor;
         this.isHidden = isHidden;
         this.keyManagement = keyMangement;
+    }
+
+    public ConfigSerialization(NdefMessage ndefMessage) {
+        byte[] bytes = ndefMessage.toByteArray();
+        Parcel parcel = Parcel.obtain();
+        parcel.readByteArray(bytes);
+        try {
+            parcel.readValue(ConfigSerialization.class.getClassLoader());
+        } finally {
+            parcel.recycle();
+        }
+
     }
 
     @Override
@@ -30,36 +47,107 @@ public class ConfigSerialization implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(SSID);
         dest.writeInt(isHidden ? 1 : 0);
-        dest.writeInt(isTwoFactor ? 1 : 0);
+        dest.writeByte((byte) (isHidden ? 1 : 0));
+        dest.writeByte((byte) (isTwoFactor ? 1 : 0));
         dest.writeString(password);
         dest.writeString(secondaryPassword);
-        dest.writeInt(keyManagement);
+        dest.writeByte((byte) keyManagement);
     }
 
     /** Implement the Parcelable interface {@hide} */
-    public static final Creator<ConfigSerialization> CREATOR =
-        new Creator<ConfigSerialization>() {
-            @Override
-            public ConfigSerialization createFromParcel(Parcel in) {
-                String SSID = in.readString();
-                int isHiddenInt = in.readInt();
-                int isTwoFactorInt = in.readInt();
-                String password = in.readString();
-                String secondaryPassword = in.readString();
-                int keyManagement = in.readInt();
+    public static final Creator<ConfigSerialization> CREATOR = new Creator<ConfigSerialization>() {
+        @Override
+        public ConfigSerialization createFromParcel(Parcel in) {
+            String SSID = in.readString();
+            boolean isHidden = in.readByte() != 0;
+            boolean isTwoFactor = in.readByte() != 0;
+            String password = in.readString();
+            String secondaryPassword = in.readString();
+            int keyManagement = in.readByte();
 
-                boolean isHidden = isHiddenInt == 1 ? true : false;
-                boolean isTwoFactor = isTwoFactorInt == 1 ? true : false;
-                ConfigSerialization config = new ConfigSerialization(SSID, password, secondaryPassword,
-                        isTwoFactor, isHidden, keyManagement);
-                return config;
+            ConfigSerialization config = new ConfigSerialization(SSID, password, secondaryPassword, isTwoFactor, isHidden, keyManagement);
+            return config;
+        }
+
+        @Override
+        public ConfigSerialization[] newArray(int size) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    };
+
+    public WifiConfiguration toWifiConfig() {
+        WifiConfiguration conf = new WifiConfiguration();
+        String passwordForConfig;
+
+        boolean isOpen = this.keyManagement == WifiConfiguration.KeyMgmt.NONE && this.password.isEmpty();
+        boolean isWEP = this.keyManagement == WifiConfiguration.KeyMgmt.NONE && (!this.password.isEmpty());
+        boolean isWPA = this.keyManagement == WifiConfiguration.KeyMgmt.WPA_PSK;
+
+        conf.SSID = this.SSID;
+        conf.status = WifiConfiguration.Status.ENABLED;
+
+        if (isTwoFactor) {
+            passwordForConfig = "HASH THE TWO PASSWORDS TOGETHER";
+        } else {
+            passwordForConfig = this.password;
+        }
+
+        if (isOpen) {
+            // For open networks
+            conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        } else if (isWEP || isWPA) {
+            conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+            conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+            conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+            conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+            conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+
+            if (isWEP) {
+                // For WEP networks
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+                conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+
+                conf.wepKeys[0] = "\"".concat(passwordForConfig).concat("\"");
+                conf.wepTxKeyIndex = 0;
+            } else if (isWPA) {
+                // For WPA and WPA2 networks
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+
+                conf.preSharedKey = "\"" + passwordForConfig + "\"";
             }
+        }
 
-            @Override
-            public ConfigSerialization[] newArray(int size) {
-                // TODO Auto-generated method stub
-                return null;
-            }
+        // For hidden networks
+        conf.hiddenSSID = this.isHidden ? true : false;
 
-        };
+        return conf;
+    }
+
+    public NdefMessage toNdefMessage() {
+        NdefMessage message = null;
+        Parcel parcel = Parcel.obtain();
+        this.writeToParcel(parcel, 0);
+        byte[] parcelByteArray = parcel.createByteArray();
+
+        try {
+            message = new NdefMessage(parcelByteArray);
+        } catch (FormatException e) {
+            // barf
+            e.printStackTrace();
+        } finally {
+            parcel.recycle();
+        }
+
+        return message;
+    }
+
 }
